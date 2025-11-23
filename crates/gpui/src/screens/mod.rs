04e42b162ce28f4b6869d7120fec703a0cc1a8ef
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2025-11-23 20:03:32 +0100

    feat: implement routing (#13)

--- crates/gpui/src/screens/mod.rs
+++ crates/gpui/src/screens/mod.rs
@@ -1,2 +1,42 @@
+use gpui::{AppContext, Context, Entity, IntoElement, ParentElement, Render, Window, div};
+use gpui_router::{Route, Routes};
+
+use crate::{components::layout::Layout, screens::login_screen::LoginScreen};
+
+pub mod login_screen;
 pub mod main_screen;
 pub mod parts;
+
+pub struct Router {
+    login_screen: Entity<LoginScreen>,
+}
+
+impl Router {
+    pub fn new(cx: &mut Context<Self>) -> Self {
+        let login_screen = cx.new(|cx| LoginScreen::new(cx));
+        Self { login_screen }
+    }
+}
+
+impl Render for Router {
+    fn render(&mut self, _: &mut Window, _: &mut Context<Self>) -> impl IntoElement {
+        div().child(
+            Routes::new()
+                .child(
+                    Route::new()
+                        .path("login")
+                        .element(self.login_screen.clone()),
+                )
+                .child(
+                    Route::new()
+                        .layout(Layout::new())
+                        .child(Route::new().index().element(div().child("Home")))
+                        .child(
+                            Route::new()
+                                .path("{*not_match}")
+                                .element(div().child("Not found")),
+                        ),
+                ),
+        )
+    }
+}
